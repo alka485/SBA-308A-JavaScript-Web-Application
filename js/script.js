f37baf5bf261719ca2e7d0a572c76525262ff2a5
Alka Sah <alkasah485@gmail.com>
2024-05-30 10:09:49 -0700

    modification in script

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const apiKey = 'b9f6c58391da8f005bd41c6735238193';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+const locationInput = document.getElementById('locationInput');
+console.log(locationInput);
+const searchButton = document.getElementById('searchButton');
+console.log(searchButton);
+const location = document.getElementById('location');
+console.log(location);
+const temperature = document.getElementById('temperature');
+console.log(temperature);
+const description = document.getElementById('description');
+console.log(description);
