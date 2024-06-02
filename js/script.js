5045331f134337dad835f271e0a13726cac731a4
Alka Sah <alkasah485@gmail.com>
2024-06-02 00:08:24 -0700

    modification in script

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const apiKey =  "b9f6c58391da8f005bd41c6735238193";
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
+const searchBox = document.querySelector(".search-box input");
+//console.log(searchBox);
 const locationInput = document.getElementById('locationInput');
 //console.log(locationInput);
 const searchButton = document.getElementById('searchButton');
-//console.log(searchButton);
+console.log(searchButton);
 const locationElement = document.getElementById('location');
 //console.log(locationElement);
 const temperatureElement = document.getElementsByClassName('temp');
@@ -12,29 +14,45 @@ const temperatureElement = document.getElementsByClassName('temp');
 const humidityElement = document.getElementsByClassName('humidity');
 const windIcon = document.querySelector(".wind");
 //console.log(windIcon);
+const weatherIcon = document.querySelector(".weather-icon");
+//console.log(weatherIcon);
 
 searchButton.addEventListener('click' , () => {
-    const location  = locationInput.value;
-    //console.log(location);
-    if (location) {
-    //    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
-    //    console.log(url);
-         fetchWeather(location);
-    }
+    //console.log("click");
+    console.log(searchBox.value);
+   fetchWeather(searchBox.value);
 })
 
-function fetchWeather(location){
+async function fetchWeather(location){
        const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
        console.log(url);
-       fetch(url) 
+      await fetch(url) 
        .then(response => response.json())
        .then(data => {
-        locationElement.textContent = data.name;
-        temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
-        humidityElement.textContent = data.main.humidity+"%";
-        windIcon.textContent = data.wind.speed + "km/hr";
+        locationElement.innerHTML = data.name;
+        temperatureElement.innerHTML= `${Math.round(data.main.temp)}°C`;
+        humidityElement.innerHTML = data.main.humidity+"%";
+        windIcon.innerHTML = data.wind.speed + "km/hr";
+
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src= '../images/images/clouds.png';
+        } else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "../images/images/clear.png";
+          }
+            else if(data.weather[0].main == "Rain"){  
+                weatherIcon.src = "../images/images/rain.png";
+            }
+            else if(data.weather[0].main == "Drizzle"){
+                    weatherIcon.src = "../images/images/drizzle.png";
+            }
+            else if(data.weather[0].main == "Mist"){
+                weatherIcon.src = "../images/images/mist.png";
+            }
+
+            document.querySelector(".weather").style.display = "block";
         
        })
+
        .catch(error => {
         console.error("Error fetching while weather data" , error);
        })
